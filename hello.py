d0504c50b7fa6f2fda6a7f147d8fb0e27a4c0171
潘江波 <panjiangbo@example.com>
2026-07-26 15:50:12 +0800

    feat: 添加年龄计算器，学习变量和输入输出

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-print("你好世界，我是娇娇的小主人")
+import sys
+import locale
+# 设置系统编码为终端支持的编码
+if sys.stdout.encoding != 'utf-8':
+    sys.stdout.reconfigure(encoding='utf-8')
+
+name = input('请输入你的名字：')
+year = input('请输入你的出生年份：')
+age = 2026 - int(year)
+print(name + '，你今年' + str(age) + '岁啦！')
